d2c930b6c32af184e6b98c6033370f9f7c57e4e4
Sergei Polonskii <sanPolonskiy@sberbank.ru>
2021-12-27 15:51:54 +0300

    - Добавил структуру - Добавлена вёрстк - Добавлено описание проекта

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require(`express`);
+const path = require(`path`);
+const app = express();
+const {DEFAULT_PORT, BUILD_PATH} = require('./constants');
+
+app.use('/', express.static(path.join(__dirname, BUILD_PATH)));
+app.use((err, req, res, next) => {
+    console.error(`Something went wrong`, err);
+    next(err);
+});
+app.get(`/`, (req, res) => res.sendFile(`${__dirname}/dist/chat.html`));
+
+app.listen(DEFAULT_PORT);
